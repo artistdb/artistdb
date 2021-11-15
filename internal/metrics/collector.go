ff8b223619f1f05410bc2fcc2e61bb5fa5cae76f
Alexander Knipping <ajknipping@gmail.com>
2021-11-15 08:48:14 +0100

    Instrument database connection (#35)
    * Instrument database connection
    
    Introduces an interface around the database connection pool as well as
    metrics. Also makes the metrics collector global.
    
    * Use exec as command name
    
    * Make conection interface private

--- internal/metrics/collector.go
+++ internal/metrics/collector.go
@@ -1,16 +1,53 @@
 package metrics
 
-import "github.com/prometheus/client_golang/prometheus"
+import (
+	"log"
+	"time"
 
-// Collector holds all metrics.
-type Collector struct{}
+	"github.com/prometheus/client_golang/prometheus"
 
-func (c *Collector) Describe(descs chan<- *prometheus.Desc) {
-	prometheus.DescribeByCollect(c, descs)
+	"github.com/obitech/artist-db/internal"
+)
+
+const (
+	subSystemDB = "database"
+)
+
+func init() {
+	Collector = newCollector()
+
+	if err := prometheus.Register(Collector); err != nil {
+		log.Fatalf("registering collector failed: %v", err)
+	}
 }
 
-func (c *Collector) Collect(metrics chan<- prometheus.Metric) {}
+var Collector *collector
 
-func NewCollector() *Collector {
-	return &Collector{}
+// collector holds all metrics.
+type collector struct {
+	dbCommandDuration *prometheus.HistogramVec
+}
+
+func (c *collector) Describe(ch chan<- *prometheus.Desc) {
+	prometheus.DescribeByCollect(c, ch)
+}
+
+func (c *collector) Collect(ch chan<- prometheus.Metric) {
+	c.dbCommandDuration.Collect(ch)
+}
+
+func newCollector() *collector {
+	return &collector{
+		dbCommandDuration: prometheus.NewHistogramVec(prometheus.HistogramOpts{
+			Namespace: internal.Name,
+			Subsystem: subSystemDB,
+			Name:      "command_duration_seconds",
+			Help:      "Observation of command durations against the database.",
+			Buckets:   prometheus.ExponentialBuckets(0.05, 2, 10),
+		}, []string{"command"}),
+	}
+}
+
+func (c *collector) ObserveCommandDuration(commandName string, duration time.Duration) {
+	c.dbCommandDuration.WithLabelValues(commandName).Observe(duration.Seconds())
 }
--- internal/server/server.go
+++ internal/server/server.go
@@ -5,39 +5,31 @@ import (
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
-	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 	"go.uber.org/zap"
 
 	"github.com/obitech/artist-db/internal/database"
-	"github.com/obitech/artist-db/internal/metrics"
 )
 
 // Server holds API handlers.
 type Server struct {
-	router  chi.Router
-	db      *database.Database
-	logger  *zap.Logger
-	metrics *metrics.Collector
+	router chi.Router
+	db     *database.Database
+	logger *zap.Logger
 }
 
 // NewServer returns a server.
 func NewServer(db *database.Database, opts ...Option) (*Server, error) {
 	srv := &Server{
-		router:  chi.NewRouter(),
-		db:      db,
-		logger:  zap.L().With(zap.String("component", "server")),
-		metrics: metrics.NewCollector(),
+		router: chi.NewRouter(),
+		db:     db,
+		logger: zap.L().With(zap.String("component", "server")),
 	}
 
 	for _, fn := range opts {
 		if err := fn(srv); err != nil {
 			return nil, fmt.Errorf("applying option failed: %w", err)
 		}
-	}
-
-	if err := prometheus.Register(srv.metrics); err != nil {
-		return nil, fmt.Errorf("registering collector failed: %w", err)
 	}
 
 	srv.router.Route("/internal", func(r chi.Router) {
