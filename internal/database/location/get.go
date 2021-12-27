997b325ab5fa5f2cd3e53c183face1a75f6040aa
Alexander Knipping <ajknipping@gmail.com>
2021-12-27 18:33:07 +0100

    Add function to retrieve Locations (#88)
    * Add function to retrieve Locations
    
    * Run go mod tidy

---
+++ internal/database/location/get.go
@@ -0,0 +1,81 @@
+package location
+
+import (
+	"context"
+	"errors"
+	"fmt"
+
+	"github.com/jackc/pgx/v4"
+
+	"github.com/obitech/artist-db/internal/database/core"
+)
+
+// GetRequest specifies the input for an  Artists query against the database.
+type GetRequest func() (string, string)
+
+// ByID requests and Artist by ID.
+func ByID(id string) GetRequest {
+	return func() (string, string) {
+		return id, "id=$1"
+	}
+}
+
+// ByName requests Locations by name.
+func ByName(name string) GetRequest {
+	return func() (string, string) {
+		return name, "name=$1"
+	}
+}
+
+func (h *Handler) Get(ctx context.Context, request GetRequest) ([]*Location, error) {
+	input, whereClause := request()
+
+	stmt := fmt.Sprintf(`
+		SELECT
+			id,
+			name
+		FROM 
+			"%s"
+		WHERE deleted_at IS NULL AND `, core.TableLocations,
+	)
+
+	stmt += whereClause
+
+	rows, err := h.conn.Query(ctx, stmt, input)
+	if err != nil {
+		if errors.Is(err, pgx.ErrNoRows) {
+			return nil, core.ErrNotFound
+		}
+
+		return nil, fmt.Errorf("query failed: %w", err)
+	}
+
+	defer rows.Close()
+
+	var locations []*Location
+
+	for rows.Next() {
+		var (
+			id   string
+			name string
+		)
+
+		if err := rows.Scan(
+			&id,
+			&name,
+		); err != nil {
+			return nil, fmt.Errorf("scanning rows failed: %w", err)
+		}
+
+		locations = append(locations, &Location{
+			ID:   id,
+			Name: name,
+		})
+	}
+
+	if len(locations) == 0 {
+		return nil, core.ErrNotFound
+	}
+
+	return locations, nil
+}
---
+++ internal/database/location/upsert.go
@@ -0,0 +1,72 @@
+package location
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"time"
+
+	"github.com/jackc/pgx/v4"
+	"go.uber.org/multierr"
+
+	"github.com/obitech/artist-db/internal/database/core"
+)
+
+// Upsert inserts or updates Locations.
+func (h *Handler) Upsert(ctx context.Context, locations ...*Location) error {
+	tx, err := h.conn.Begin(ctx)
+	if err != nil {
+		return fmt.Errorf("creating tx failed: %w", err)
+	}
+
+	defer core.RollbackAndLogError(ctx, tx, h.logger)
+
+	var mErr error
+	for _, location := range locations {
+		if err := h.upsert(ctx, tx, location); err != nil {
+			if errors.Is(err, pgx.ErrTxClosed) {
+				return fmt.Errorf("insert aborted, tx cancelled: %w", err)
+			}
+
+			mErr = multierr.Append(mErr, err)
+		}
+	}
+
+	if err := tx.Commit(ctx); err != nil {
+		return fmt.Errorf("commiting tx failed: %w", err)
+	}
+
+	return mErr
+}
+
+func (h *Handler) upsert(ctx context.Context, tx pgx.Tx, location *Location) error {
+	start := time.Now().UTC()
+
+	stmt := fmt.Sprintf(`
+		INSERT INTO "%s"
+			(
+				id,
+				created_at,
+				updated_at,
+				name
+			)
+		VALUES 
+			($1, $2, $3, $4)
+		ON CONFLICT 
+			(id)
+		DO UPDATE SET
+			updated_at=$3,
+			name=$4,
+			deleted_at=NULL`, core.TableLocations)
+
+	if _, err := tx.Exec(ctx, stmt,
+		location.ID,   // $1
+		start,         // $2
+		start,         // $3
+		location.Name, // $4
+	); err != nil {
+		return err
+	}
+
+	return nil
+}
