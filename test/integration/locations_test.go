d6a1455f385d65e3d06c7dbda9319c97f8c2916a
Alexander Knipping <ajknipping@mailbox.org>
2021-12-28 10:46:47 +0100

    Add function to delete Location by ID

--- test/integration/locations_test.go
+++ test/integration/locations_test.go
@@ -73,4 +73,41 @@ func Test_LocationsIntegration(t *testing.T) {
 			assert.Equal(t, updatedAt, createdAt)
 		})
 	})
+
+	t.Run("deleting location works", func(t *testing.T) {
+		t.Run("invalid ID throws error", func(t *testing.T) {
+			require.ErrorIs(t, db.LocationHandler.DeleteByID(ctx, "foo"), core.ErrInvalidUUID)
+		})
+
+		t.Run("deleting unknown location throws error", func(t *testing.T) {
+			require.ErrorIs(t, db.LocationHandler.DeleteByID(ctx, uuid.New().String()), core.ErrNotFound)
+		})
+
+		t.Run("location is created", func(t *testing.T) {
+			t.Run("create", func(t *testing.T) {
+				require.NoError(t, db.LocationHandler.Upsert(ctx, locations[0]))
+			})
+
+			t.Run("verify", func(t *testing.T) {
+				locs, err := db.LocationHandler.Get(ctx, location.ByID(locations[0].ID))
+				require.NoError(t, err)
+
+				assert.Len(t, locs, 1)
+				assert.Equal(t, locations[0].ID, locs[0].ID)
+			})
+		})
+
+		t.Run("location is deleted", func(t *testing.T) {
+			t.Run("delete", func(t *testing.T) {
+				require.NoError(t, db.LocationHandler.DeleteByID(ctx, locations[0].ID))
+			})
+
+			t.Run("verify", func(t *testing.T) {
+				locs, err := db.LocationHandler.Get(ctx, location.ByID(locations[0].ID))
+				require.ErrorIs(t, err, core.ErrNotFound)
+
+				assert.Len(t, locs, 0)
+			})
+		})
+	})
 }
