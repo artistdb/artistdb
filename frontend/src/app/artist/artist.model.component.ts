ea97f73da579a6b55544d30f89aca4606519837c
Robert Schulze <39327898+softbobo@users.noreply.github.com>
2022-01-28 08:48:02 +0100

    Artist view (#120)

--- frontend/src/app/artist/artist.model.component.ts
+++ frontend/src/app/artist/artist.model.component.ts
@@ -2,7 +2,7 @@ export interface ArtistInput {
     firstName: string;
     lastName: string;
     artistName?: string;
-    pronouns?: [string];
+    pronouns?: string[];
     // make dateOfBirth?: [Date];
     placeOfBirth?: string;
     nationality?: string;
@@ -19,10 +19,28 @@ export const MOCK_ARTISTS: ArtistInput[] = [
         firstName: "Jens",
         lastName: "Rainer",
         artistName: "jens-rainer",
+        pronouns: ["he", "him"],
+        placeOfBirth: "drüben",
+        nationality: "drüber",
+        language: "zeug",
+        facebook: "www.meta.com/bs",
+        instagram: "www.meta.com/instant_bs",
+        bandcamp: "www.bandcamp.com/zeug_xxx",
+        bioGer: "Ich komm von drüben",
+        bioEn: "I came from yonder",
     },
     {
         firstName: "Kai",
         lastName: "Uwe",
-        artistName: "uwe-kai"
+        artistName: "uwe-kai",
+        pronouns: ["they", "them"],
+        placeOfBirth: "drüben",
+        nationality: "drüber",
+        language: "zeug",
+        facebook: "www.meta.com/bs",
+        instagram: "www.meta.com/instant_bs",
+        bandcamp: "www.bandcamp.com/zeug_xxx",
+        bioGer: "Ich komm von drüben",
+        bioEn: "I came from yonder",
     },
 ]
