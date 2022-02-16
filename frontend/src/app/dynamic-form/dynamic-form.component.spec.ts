cb7286bfab8da9dd9756c9e8d20511cb1233b3ca
Robert Schulze <robert_schulze@posteo.de>
2022-02-16 16:49:31 +0100

    Fix imports in dynamic-form specs

--- frontend/src/app/dynamic-form/dynamic-form.component.spec.ts
+++ frontend/src/app/dynamic-form/dynamic-form.component.spec.ts
@@ -1,20 +1,20 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { FormComponent } from './form.component';
+import { DynamicFormComponent } from './dynamic-form.component';
 
-describe('FormComponent', () => {
-  let component: FormComponent;
-  let fixture: ComponentFixture<FormComponent>;
+describe('DynamicFormComponent', () => {
+  let component: DynamicFormComponent;
+  let fixture: ComponentFixture<DynamicFormComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ FormComponent ]
+      declarations: [ DynamicFormComponent ]
     })
     .compileComponents();
   });
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(FormComponent);
+    fixture = TestBed.createComponent(DynamicFormComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
