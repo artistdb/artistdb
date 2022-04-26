a6288cdd8f35d153d6c0d01aa8de862b9bbc66a0
Robert Schulze <robert_schulze@posteo.de>
2022-04-26 07:24:08 +0200

    remove dynamic-form-field unit test

--- frontend/src/app/dynamic-form/dynamic-form-field/dynamic-form-field.component.spec.ts
+++ frontend/src/app/dynamic-form/dynamic-form-field/dynamic-form-field.component.spec.ts
@@ -18,8 +18,4 @@ describe('DynamicFormFieldComponent', () => {
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
-
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
 });
