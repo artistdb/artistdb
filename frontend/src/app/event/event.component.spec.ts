188aad6e845fcc5db4a181b28e658a0d03eaf97e
Robert Schulze <robert_schulze@posteo.de>
2022-06-27 17:38:22 +0200

    Add unit tests

--- frontend/src/app/event/event.component.spec.ts
+++ frontend/src/app/event/event.component.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
 
 import { EventComponent } from './event.component';
 
@@ -22,4 +23,39 @@ describe('EventComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should show event input form upon clicking button', () => {
+    const b = fixture.debugElement.query(By.css('.--new-event'));
+    b.triggerEventHandler('click', null);
+    const form: HTMLElement = fixture.nativeElement.querySelector('app-event-form')
+    expect(form).toBeDefined();
+  })
+
+  it('should have a name field', () => {
+    const b = fixture.debugElement.query(By.css('.--new-event'));
+    b.triggerEventHandler('click', null);
+    const f = fixture.debugElement.query(By.css('label[for="name"]'))
+    expect(f).toBeDefined()
+  })
+
+  it('should have a time field', () => {
+    const b = fixture.debugElement.query(By.css('.--new-event'));
+    b.triggerEventHandler('click', null);
+    const f = fixture.debugElement.query(By.css('label[for="Start Date/Time"]'))
+    expect(f).toBeDefined()
+  })
+
+  it('should have a location field', () => {
+    const b = fixture.debugElement.query(By.css('.--new-event'));
+    b.triggerEventHandler('click', null);
+    const f = fixture.debugElement.query(By.css('label[for="Location"]'))
+    expect(f).toBeDefined()
+  })
+
+  it('should have a artists field', () => {
+    const b = fixture.debugElement.query(By.css('.--new-event'));
+    b.triggerEventHandler('click', null);
+    const f = fixture.debugElement.query(By.css('label[for="Invited Artists"]'))
+    expect(f).toBeDefined()
+  })
 });
